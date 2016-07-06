edbc99477bee5f6d907387d8da7bf81a65125efa
fs1990qq <fs1990qq@sina.com>
2016-07-06 10:07:26 +0800

    fff

--- ProjectOne/ProjectOne/ViewController.m
+++ ProjectOne/ProjectOne/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabel *test7622222 = [[UILabel alloc]init];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
